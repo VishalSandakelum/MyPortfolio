ca5be8294c80993014c1513bac9be0ac84db3cdf
vishalgit2005 <vishalsandakelum20@gmail.com>
2023-10-10 11:03:46 +0530

    modify

--- javascript.js
+++ javascript.js
@@ -2,10 +2,10 @@ const loadingpage = document.querySelector('#loadingpage');
 
 window.addEventListener('load', function() {
     setTimeout(function() {
-        //loadingpage.style.opacity = '0';
+        loadingpage.style.opacity = '0';
         setTimeout(function() {
-            //loadingpage.style.display = 'none';
+            loadingpage.style.display = 'none';
         }, 1000); 
-        //console.log('Window is fully loaded');
+        console.log('Window is fully loaded');
     }, 0);
 });
