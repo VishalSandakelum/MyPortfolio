3698d331fea2655a98471483bf97a4dbd7a8157b
vishalgit2005 <vishalsandakelum20@gmail.com>
2023-10-11 20:39:40 +0530

    modify

--- assigments/JS/InMemory POS/db/DB.js
+++ assigments/JS/InMemory POS/db/DB.js
@@ -3,3 +3,9 @@ let customer = new Array(
     {name: 'Kasun',cusid: 'C00-002',cusnomber: '+94 76 2656 420',cussalry: '100000.00'},
     {name: 'Amal',cusid: 'C00-003',cusnomber: '+94 76 2656 420',cussalry: '150000.00'}
 );
+
+let item = new Array(
+    {itmcode: 'I00-001',itmname: 'Sun Light',itmprice: '150',itmqty: '50'},
+    {itmcode: 'I00-002',itmname: 'Signal',itmprice: '180',itmqty: '60'},
+    {itmcode: 'I00-003',itmname: 'Milk Packet',itmprice: '550',itmqty: '60'}
+);
