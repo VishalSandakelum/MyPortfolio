cff45b9bc45264b0357262b949aab8f0de9a9148
vishalgit2005 <vishalsandakelum20@gmail.com>
2023-10-12 22:14:15 +0530

    modify

--- assigments/JS/InMemory POS/controllers/CustomerController.js
+++ assigments/JS/InMemory POS/controllers/CustomerController.js
@@ -23,24 +23,31 @@
           //Data Save
           customerAr.push(newCustomer);
           //Data Add For Table
-          datarow();
+          getAllData();
           console.log(customerAr);
           
         });
 
         //This Function For View All Customer
         viewallbtn.addEventListener("click",function(){
+          $('#cusdatatable td').parent().remove();
           getAllData();
         });
 
         function getAllData(){
           for(i in customerAr){
             console.log(customerAr[i]);
+            let custId = customerAr[i].cusId;
+            let custnam = customerAr[i].name;
+            let custnom = customerAr[i].cusnomber;
+            let custsal = customerAr[i].cussalry;
+
+            datarow(custId,custnam,custnom,custsal);
           }
         }
 
         //This Function For All Data Add Add To The Table
-        function datarow(){
+        function datarow(id,nam,nomb,sal){
 
           const tr = document.createElement("tr");
           const td1 = document.createElement("td");
@@ -48,12 +55,14 @@
           const td3 = document.createElement("td");
           const td4 = document.createElement("td");
 
-          td1.textContent=cusnam.value;
-          td2.textContent=cusId.value;
-          td3.textContent=cusAddress.value;
-          td4.textContent=cusSalary.value;
+          td1.textContent = id;
+          td2.textContent = nam;
+          td3.textContent = nomb;
+          td4.textContent = sal;
 
+          $('#cusdatatable').addClass("table");
           tr.append(td1,td2,td3,td4);
+          $('#cusdatatable').append(tr);
           document.querySelector("#cusdatatable").append(tr);
 
         }
