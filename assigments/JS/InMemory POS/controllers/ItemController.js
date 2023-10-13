72b5ce021996af0e44c7bde5cde241b9d330fe8f
vishalgit2005 <vishalsandakelum20@gmail.com>
2023-10-13 21:53:28 +0530

    modify

--- assigments/JS/InMemory POS/controllers/ItemController.js
+++ assigments/JS/InMemory POS/controllers/ItemController.js
@@ -8,3 +8,36 @@ $('#itemsavebtn').click(function(){
 
     item.push(newItem);
 });
+
+//This Function For View All Item
+$('#itmviewallbtn').click(function(){
+    $('#itmtable td').parent().remove();
+    getAllData();
+ });
+
+ //This Function For Get All Data From Array
+ function getAllData(){
+    for(i in item){
+      console.log(item[i]);
+      let itmcode = item[i].itmcode;
+      let itmname = item[i].itmname;
+      let itmqty = item[i].itmqty;
+      let itmprice = item[i].itmprice;
+
+      datarow(itmcode,itmname,itmqty,itmprice);
+    }
+  }
+
+  //This Function For All Data Add Add To The Table
+  function datarow(itmcode,itmname,itmqty,itmprice){
+
+    let row = `<tr>
+                <td>${itmcode}</td>
+                <td>${itmname}</td>
+                <td>${itmprice}</td>
+                <td>${itmqty}</td>
+              </tr>`;
+
+    $("#itmtable").append(row);
+    setClickeventForTable();
+  }
