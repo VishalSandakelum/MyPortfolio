11f3cfd9df7367a1be77679f72ddfa7896e4769f
vishalgit2005 <vishalsandakelum20@gmail.com>
2023-10-13 21:21:33 +0530

    modify

---
+++ assigments/JS/InMemory POS/controllers/ItemController.js
@@ -0,0 +1,8 @@
+$('#itemsavebtn').click(function(){
+    let newItem = Object.assign({},Item);
+
+    newItem.itmcode = $('');
+    newItem.itmname = $('');
+    newItem.itmqty = $('');
+    newItem.itmprice = $('');
+});
